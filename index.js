8fc0fccc47bc9ed83fe5d893e8ce2e9efb914191
longchapa <rolanchaparro.cos@gmail.com>
2020-01-17 17:01:30 -0500

    respuesta automatica

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/webhook', (req,res)=>{
 })
 
 app.post('/webhook', (req,res)=>{
-    const webhook_event = req.body.entry[0]
+    const webhook_event = req.body.entry
     if(webhook_event.messaging){
         webhook_event.messaging.forEach(event =>{
             handleEvent(event.sender.id, event)
