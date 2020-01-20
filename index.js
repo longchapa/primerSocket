fe14c8e68eafd947f6b7b29d6f76efc654464ec0
longchapa <rolanchaparro.cos@gmail.com>
2020-01-20 15:21:34 -0500

    sockets

--- index.js
+++ index.js
@@ -9,16 +9,11 @@ const request = require('request');
 const io = require('socket.io')(server)
 const access_token = "EAAjISRRWe2UBAEegIbFw8iiU22hiFu7HtAMn32sOTy89pWzxLYJbMyQ5MJFVYr5TUjBF1Q0R1mOm9AqgyVbXNZAlV5LmAum1ZBAAz0UlcHFKZBsBKPSWZBMpN9BomM7LBeQ272byo5WuUIZAqZChXTietQfQ5RffgKTPG2FcXjkjNzvl9647R6MUwHIJEvBNoZD"
 
-
-
 const PORT = process.env.PORT || 5000
+
 app.use(bodyParser.json());
 
-app.get('/', (req,res)=>{
-    res.status(200).send(`Hola bb`)
-})
-
-app.use('/socket', express.static('public'))
+app.use(express.static('public'))
 
 app.get('/webhook', function(req, res){
     if(req.query['hub.verify_token'] === 'pugpizza_token'){
