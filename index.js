5660c8a60fa3275f2d1331f6f3b473b3adc297e9
longchapa <rolanchaparro.cos@gmail.com>
2020-01-21 11:33:23 -0500

    problemas de cors

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ const io = require('socket.io')(server)
 const access_token = "EAAjISRRWe2UBAEegIbFw8iiU22hiFu7HtAMn32sOTy89pWzxLYJbMyQ5MJFVYr5TUjBF1Q0R1mOm9AqgyVbXNZAlV5LmAum1ZBAAz0UlcHFKZBsBKPSWZBMpN9BomM7LBeQ272byo5WuUIZAqZChXTietQfQ5RffgKTPG2FcXjkjNzvl9647R6MUwHIJEvBNoZD"
 const cors = require('cors')
 
+app.use(cors({
+    origin: '190.14.248.146'
+}))
+
 const PORT = process.env.PORT || 5000
 
 app.use(bodyParser.json());
 
-app.use(cors({
-    origin: '190.14.248.146'
-}))
+
 
 
 app.get('/', (req,res)=>{
