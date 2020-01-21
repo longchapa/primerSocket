d4713ed4ad70ee7e05c6b9d675112d32b30641a6
longchapa <rolanchaparro.cos@gmail.com>
2020-01-21 08:03:49 -0500

    Elimina la carpeta public

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const PORT = process.env.PORT || 5000
 
 app.use(bodyParser.json());
 
-app.use(express.static('public'))
+app.get('/', (req,res)=>{
+    res.status(200).send(`Hola mundo!!`)
+})
 
 app.get('/webhook', function(req, res){
     if(req.query['hub.verify_token'] === 'pugpizza_token'){
