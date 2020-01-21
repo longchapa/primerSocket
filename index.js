ea853b1fe0aab0627d395437f5ddb6a88504f980
longchapa <rolanchaparro.cos@gmail.com>
2020-01-21 11:42:34 -0500

    problemas de cors

--- index.js
+++ index.js
@@ -15,8 +15,11 @@ const cors = require('cors')
 const PORT = process.env.PORT || 5000
 
 app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin', '*')
-    next()
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
 })
 
 app.use(bodyParser.json());
