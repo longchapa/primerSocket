ed0e5f9aa0b800d8dac7c3281871a82f684a31f3
longchapa <rolanchaparro.cos@gmail.com>
2020-01-21 16:43:04 -0500

    Prueba

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const request = require('request');
 const io = require('socket.io')(server)
 const access_token = "EAAjISRRWe2UBAEegIbFw8iiU22hiFu7HtAMn32sOTy89pWzxLYJbMyQ5MJFVYr5TUjBF1Q0R1mOm9AqgyVbXNZAlV5LmAum1ZBAAz0UlcHFKZBsBKPSWZBMpN9BomM7LBeQ272byo5WuUIZAqZChXTietQfQ5RffgKTPG2FcXjkjNzvl9647R6MUwHIJEvBNoZD"
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
+
 
 app.use(bodyParser.json());
 
@@ -17,34 +18,29 @@ app.get('/', (req, res) => {
     res.status(200).send(`Hola mundo!!`)
 })
 
-try{
-    const messages = [{
-        author: "Carlos",
-        text: "Hola, que tal?",
-        id: 1
-    }, {
-        author: "Pepe",
-        text: "Muy bien! y tu?"
-    }, {
-        author: "Paco",
-        text: "Genial!"
-    }]
-    
-    
-    io.on('connection', function (socket) {
-        console.log(`Cliente conectado`)
-        socket.emit('messages', messages)
-    
-        socket.on('new-message', (data) => {
-            console.log(`Se metio!!!`)
-            messages.push(data)
-    
-            io.sockets.emit('messages', messages)
-        })
+const messages = [{
+    author: "Carlos",
+    text: "Hola, que tal?",
+    id: 1
+}, {
+    author: "Pepe",
+    text: "Muy bien! y tu?"
+}, {
+    author: "Paco",
+    text: "Genial!"
+}]
+
+io.on('connection', function (socket) {
+    console.log(`Cliente conectado`)
+    socket.emit('messages', messages)
+
+    socket.on('new-message', (data) => {
+        console.log(`Se metio!!!`)
+        messages.push(data)
+
+        io.sockets.emit('messages', messages)
     })
-}catch(err){
-    console.log(err)
-}
+})
 
 // app.get('/webhook', function(req, res){
 //     if(req.query['hub.verify_token'] === 'pugpizza_token'){
