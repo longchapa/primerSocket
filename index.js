47a0782924f7baa3dd34b609b8ce36e820f6c2b3
longchapa <rolanchaparro.cos@gmail.com>
2020-01-27 14:30:15 -0500

    Error por carga de modulo externo

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/webhook', (req, res)=>{
     if(webhook_event.messaging){
         webhook_event.messaging.forEach(event=>{
             // handleEvent(event.sender.id, event)
-            console.log(event)
+            console.log(event.message.text)
         })
         res.sendStatus(200)
     }else{
